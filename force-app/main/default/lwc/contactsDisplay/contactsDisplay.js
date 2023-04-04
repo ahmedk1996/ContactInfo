4e593213731e017e32620ccf91090f8a2fd4e855
Ahmed Khattab <ahmed.khattab@ised-isde.gc.ca>
2023-04-04 07:11:52 -0400

    Initial commit

---
+++ force-app/main/default/lwc/contactsDisplay/contactsDisplay.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+import CONTACT from '@salesforce/schema/Contact';
+import CONTACT_FIRSTNAME from '@salesforce/schema/Contact.FirstName';
+import CONTACT_LASTNAME from '@salesforce/schema/Contact.LastName';
+import CONTACT_EMAIL from '@salesforce/schema/Contact.Email';
+
+export default class ContactsDisplay extends LightningElement {
+
+    objectApiName=CONTACT;
+    fields = [CONTACT_FIRSTNAME, CONTACT_LASTNAME, CONTACT_EMAIL];
+    
+    handleCLicks(){
+
+    }
+}
